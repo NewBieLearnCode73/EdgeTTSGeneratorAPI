21c93b0508a79b5e2060e8c2df967f8c1b370e11
DinhChieu <ndchieu73@gmail.com>
2025-07-25 19:26:55 +0700

    Add validate request!

--- middlewares/validators/AuthValidator.js
+++ middlewares/validators/AuthValidator.js
@@ -1,4 +1,31 @@
-const Joi = require("joi")
+const Joi = require("joi");
+const HTTP_STATUS_CODE = require("http-status-codes");
+const ApiError = require("../../utils/ApiError");
 
 const registerSchema = Joi.object({
-    username : Joi.string().alphanum().min(6).max(20).required()})
+  username: Joi.string().alphanum().min(6).max(20).required(),
+  password: Joi.string().min(6).required(),
+  email: Joi.string()
+    .pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+    .required(),
+});
+
+const loginSchema = Joi.object({
+  username: Joi.string().alphanum().min(6).max(20).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const validate = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return next(new ApiError(HTTP_STATUS_CODE.StatusCodes.BAD_REQUEST, error.details[0].message));
+    }
+    next();
+  };
+};
+
+module.exports = {
+  validateRegister: validate(registerSchema),
+  validateLogin: validate(loginSchema),
+};
--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const AuthController = require("../controllers/AuthController")
+const { validateRegister, validateLogin } = require("../middlewares/validators/AuthValidator");
 
-const AuthRouter = express.Router()
+const AuthRouter = express.Router();
 
-AuthRouter.post("/register", AuthController.registerUser)
-AuthRouter.post("/login", AuthController.loginUser)
+AuthRouter.post("/register", validateRegister, AuthController.registerUser);
+AuthRouter.post("/login", validateLogin, AuthController.loginUser);
 
 module.exports = AuthRouter
